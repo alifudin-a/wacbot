0fb2bef11fa54b98922dbdd020af46ecd487c8db
alifudin-a <alifudinalfarizi@gmail.com>
2022-01-06 10:48:47 +0700

    small qrcode di terminal

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ client.initialize();
 
 client.on("qr", (qr) => {
   // NOTE: This event will not be fired if a session is specified.
-  console.log("QR RECEIVED", qr);
-  qrcodeTerminal.generate(qr);
+  console.log("QR RECEIVED");
+  qrcodeTerminal.generate(qr, { small: true });
   //   qrcode.toDataURL(qr, (err, url) => {
   //   qrcodeTerminal.generate(url)
   // console.log("URL: ", url);
@@ -45,15 +45,41 @@ client.on("ready", () => {
 });
 
 client.on("message", async (msg) => {
+  // filter pesan masuk agar tidak membalas pesan dari status
   if (msg.isStatus) return;
 
+  // ubah semua keyword ke lowercase
+  const keyword = msg.body.toLowerCase();
+  console.log(keyword);
+
+  // ambil tipe / detail chat
   const chat = msg.getChat();
 
+  // filter pesan masuk agar tidak membalas pesan dari grup
   if (!(await chat).isGroup) {
     console.log("Chat bukan dari grup");
-    if (msg.body === "ping") {
-      client.sendMessage("pong");
+    if (keyword === "ping") {
+      client.sendMessage(msg.from, "pong");
+    } else if (keyword === "form") {
+      client.sendMessage(
+        msg.from,
+        "Hi! Terimakasih sudah chat Helpdesk IT, tim Helpdesk IT akan bantu secepatnya yah."
+      );
+      client.sendMessage(
+        msg.from,
+        "Mohon berikan penjelasan mendetail terkait kendala kamu dan berikan data pendukungnya."
+      );
+      client.sendMessage(
+        msg.from,
+        "**[Form]* \n**Nama* : \n**NPM* : \n**Prodi* : \n**Kendala* :"
+      );
+    } else if (keyword.match(/\[[^\]]*]/g)) {
+      client.sendMessage(msg.from, "Terima kasih!");
     }
   }
-  console.log("MESSAGE RECEIVED", msg);
+  // console.log("MESSAGE RECEIVED", msg);
+});
+
+client.on("disconnected", (reason) => {
+  console.log("Client was logged out", reason);
 });
